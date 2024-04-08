fd8d4f0f49ca9e071dadea389bd180e3b830bc48
Brandon Delehoy <brandon.delehoy@gmail.com>
2024-04-07 17:20:25 -0700

    Add support for separator strings appearing in song titles

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 from mutagen.flac import FLAC
 from pathlib import Path
 
-MUSIC_FOLDER = Path("")
+MUSIC_FOLDER = Path("C:\\Users\\your_username\\Downloads\\extracted_music_folder")
+
+# "Artist - Song"
+#        ^^^
+ARTIST_SONG_SEPARATOR_STRING = " - "
 
 if __name__ == "__main__":
     for f in MUSIC_FOLDER.iterdir():
-        if len(f.suffix) > 1 and f.suffix[1:].upper() == "FLAC":
+        if f.is_file() and len(f.suffix) > 1 and f.suffix[1:].upper() == "FLAC":
             print(f"Processing file: {f}")
             metadata = FLAC(f)
             title = metadata["title"]
-            if type(title) == list and len(title) > 0:
+            if isinstance(title, list) and len(title) > 0:
                 title = title[0]
 
-            # Find title tags that follow the format "{artist} - {song}"
-            # Will not work with artists or songs that contain the string " - "
-            if " - " in title:
-                title_parts = title.split(" - ")
-                if len(title_parts) == 2:
-                    new_song_title = title_parts[1]
-                    new_artist = title_parts[0]
+            if ARTIST_SONG_SEPARATOR_STRING in title:
+                title_parts = title.split(ARTIST_SONG_SEPARATOR_STRING)
 
-                    metadata["title"] = new_song_title
-                    metadata["artist"] = new_artist
-                    metadata.save()
-                    print(f"    *** Changed metadata for {new_artist} - \"{new_song_title}\"")
+                new_artist = title_parts[0]
+
+                # Get song title
+                if len(title_parts) == 2:
+                    # Cleanly split into artist and song title
+                    new_song_title = title_parts[1]
+                elif len(title_parts) > 2:
+                    # Split into multiple tokens (uncommon)
+                    new_song_title = ARTIST_SONG_SEPARATOR_STRING.join(title_parts[1:])
                 else:
-                    print(f"    Incompatible title (unexpected split): {title}")
+                    print(f"    Incompatible title (unexpected song title tokens): {title}")
+                    continue
+
+                metadata["artist"] = new_artist
+                metadata["title"] = new_song_title
+                metadata.save()
+                print(f"    *** Changed metadata for {new_artist} - \"{new_song_title}\"")
             else:
-                print(f"    Incompatible title (nothing to split): {title}")
+                print(f"    Incompatible title (couldn't split; incorrect separator string?): {title}")
